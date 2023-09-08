f86daf1bac2f9b6f30b6a9de01bd59c259526c49
AlvinScript <332059317@qq.com>
2023-09-08 18:56:22 +0800

    add

---
+++ src/main/java/leetcode/Q704_二分查找/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.Q704_二分查找;
+
+/**
+ * <p>描 述：</p>
+ *
+ * @author cxw (332059317@qq.com)
+ * @version 1.0.0
+ * @since 2023/9/8  13:14
+ */
+public class Solution {
+    /**
+     * 二分查找
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
