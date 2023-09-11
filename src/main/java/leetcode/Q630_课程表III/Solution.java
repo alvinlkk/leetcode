b6d7ae38436081b314c0a7720898893e01df4c0f
AlvinScript <332059317@qq.com>
2023-09-11 21:03:13 +0800

    add

---
+++ src/main/java/leetcode/Q630_课程表III/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.Q630_课程表III;
+
+/**
+ * <p>描 述：</p>
+ * 这里有 n 门不同的在线课程，按从 1 到 n 编号。给你一个数组 courses ，
+ * 其中 courses[i] = [durationi, lastDayi] 表示第 i 门课将会 持续 上 durationi 天课，
+ * 并且必须在不晚于 lastDayi 的时候完成。
+ *
+ * 你的学期从第 1 天开始。且不能同时修读两门及两门以上的课程。
+ *
+ * 返回你最多可以修读的课程数目。
+ *
+ * @author cxw (332059317@qq.com)
+ * @version 1.0.0
+ * @since 2023/9/11  20:35
+ */
+public class Solution {
+
+    /**
+     * int costDays < lastDayi
+     * costDays = durationi 相加
+     * @param courses
+     * @return
+     */
+    public int scheduleCourse(int[][] courses) {
+        return 0;
+    }
+}
