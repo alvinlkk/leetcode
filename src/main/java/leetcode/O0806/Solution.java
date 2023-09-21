dc5482bb2ab036869b3322f3142902b9033c9e1c
AlvinScript <332059317@qq.com>
2023-09-21 21:13:30 +0800

    add

---
+++ src/main/java/leetcode/O0806/Solution.java
@@ -0,0 +1,41 @@
+package leetcode.O0806;
+
+import java.util.List;
+
+/**
+ * <p>描 述：</p>
+ * 在经典汉诺塔问题中，有 3 根柱子及 N 个不同大小的穿孔圆盘，盘子可以滑入任意一根柱子。一开始，所有盘子自上而下按升序依次套在第一根柱子上(即每一个盘子只能放在更大的盘子上面)。移动圆盘时受到以下限制:
+ * (1) 每次只能移动一个盘子;
+ * (2) 盘子只能从柱子顶端滑出移到下一根柱子;
+ * (3) 盘子只能叠在比它大的盘子上。
+ *
+ * 请编写程序，用栈将所有盘子从第一根柱子移到最后一根柱子。
+ *
+ * 你需要原地修改栈。
+ *
+ * @author cxw (332059317@qq.com)
+ * @version 1.0.0
+ * @since 2023/9/21  20:40
+ */
+public class Solution {
+
+    public void hanota(List<Integer> A, List<Integer> B, List<Integer> C) {
+
+
+
+    }
+
+    /**
+     * 移动
+     * @param n 数量
+     * @param A 源柱子
+     * @param B 中间柱子
+     * @param C 目标柱子
+     */
+    private void move(int n, List<Integer> A, List<Integer> B, List<Integer> C) {
+
+
+        move(n -1, A, C, B);
+
+    }
+}
--- src/main/java/offer/Q10_青蛙跳台阶问题/Solution.java
+++ src/main/java/offer/Q10_青蛙跳台阶问题/Solution.java
@@ -1,5 +1,8 @@
 package offer.Q10_青蛙跳台阶问题;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * <p>描 述：</p>
  * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
